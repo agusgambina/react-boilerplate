96d2f12a4fa39267a93f6180c406140e74e92f03
Agustin Gambina <agusgambina@gmail.com>
2018-04-20 12:06:41 -0300

    Change the tests setup, remove test_helper.js, added Enzyme

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,15 +1,27 @@
-import { renderComponent, expect } from '../test_helper';
+// import App from '../../src/components/app';
+//
+// describe('App', () => {
+//   let component;
+//
+//   beforeEach(() => {
+//     component = renderComponent(App);
+//   });
+//
+//   it('renders something', () => {
+//     expect(component).to.exist;
+//   });
+//
+// });
+
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import { spy } from 'sinon';
 import App from '../../src/components/app';
 
-describe('App', () => {
-  let component;
-
-  beforeEach(() => {
-    component = renderComponent(App);
+describe('<App />', () => {
+  it('should exist', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).to.exist;
   });
-
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
-
 });
